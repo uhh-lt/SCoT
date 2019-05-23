8a6d550b98ce341bba5bd0c4c968884949f7431a
Inga Kempfert <5kempfer@informatik.uni-hamburg.de>
2019-05-23 19:23:11 +0200

    add Javascript function for rendering graph from parameters in HTML input via URL

--- src/scot.py
+++ src/scot.py
@@ -1,16 +1,19 @@
 # TODOs
 # 1. Improve database query performance
 # 2. Rework Flask architecture
-# 3. Check CW implementation for WSI
+# 3. How to include the target word 
+# and show the relationships between words better?
 
 from flask import Flask, jsonify, render_template, request, Response
 from flask_cors import CORS
 
 from db import Database
 import chineseWhispers
+import urllib.parse
 import json
 
 DEBUG = True
+PARAMETERS = {}
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -20,31 +23,29 @@ app.config.from_object(__name__)
 def index():
 	return render_template('index.html')
 
-@app.route('/graph')
-def get_clustered_graph(
-	target_word="freedom/NN",
-	start_year=1909,
-	end_year=1972,
-	paradigms=5,
-	pparadigms=5,
-	density=10,
-	time_diff=False
-	):
-	#if request.method == "POST":
-		# if time_diff == False
+#/<target_word>/<int:start_year>/<int:end_year>/<int:direct_neighbours>/<int:indirect_neighbours>/<int:density>/<mode>
+@app.route('/sense_graph/<path:target_word>/<int:start_year>/<int:end_year>/<int:direct_neighbours>/<int:density>/<mode>')
+def get_clustered_graph(target_word, start_year, end_year, direct_neighbours, density, mode):
+	target_word = str(urllib.parse.unquote(target_word))
+	print(target_word)
+	paradigms = direct_neighbours
+	if mode == "False":
+		time_diff = False
+	else:
+		time_diff = True
+
 	def clusters(
 		target_word,
 		start_year,
 		end_year,
 		paradigms,
-		pparadigms,
 		density,
 		time_diff
 		):
 		db = Database()
 		time_ids = db.get_time_ids(start_year, end_year)
 		#print(time_ids)
-		nodes = db.get_nodes(time_diff, target_word, paradigms, pparadigms, time_ids)
+		nodes = db.get_nodes(time_diff, target_word, paradigms, time_ids)
 		#print(nodes)
 		edges = db.get_edges(time_diff, nodes, density, time_ids)
 		return chineseWhispers.chinese_whispers(nodes, edges)
@@ -53,7 +54,6 @@ def get_clustered_graph(
 		start_year,
 		end_year,
 		paradigms,
-		pparadigms,
 		density,
 		time_diff)
 
