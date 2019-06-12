4af8e03b399b86c82635024d98b1a441741f5ee7
Inga Kempfert <5kempfer@informatik.uni-hamburg.de>
2019-06-12 11:33:56 +0200

    render nodes and fade unlinked nodes on mouseover

--- src_vue/static/render_graph_from_file.js
+++ src_vue/static/render_graph_from_file.js
@@ -44,9 +44,8 @@ function render_graph_from_file(graph) {
 			.attr("stroke", "#999")
 			.attr("stroke-opacity", 0.6)
 			.attr("class", "link")
-		.selectAll("line");
-
-	link.data(links).enter().append("line")
+		.selectAll("line")
+		.data(links).enter().append("line")
 		.attr("x1", function(d) { return d.__data__.source.x })
 		.attr("y1", function(d) { return d.__data__.source.y })
 		.attr("x2", function(d) { return d.__data__.target.x })
@@ -58,10 +57,79 @@ function render_graph_from_file(graph) {
 			.attr("stroke-width", 1.5)
 			.attr("class", "node")
 		.selectAll("g")
-
-	node.data(nodes).enter().append("g")
+		.data(nodes).enter().append("g")
+			.attr("transform", function(d) {return "translate(" + d.node.__data__.x + "," + d.node.__data__.y + ")"; } )
+			.attr("selected", function(d) { return d.node.__data__.selected; })
+			.attr("id", function(d) { return d.node.__data__.id; })
 	    .on("mousedown", mousedowned)
-	    .call(drag_node)
+	    //	.call(drag_node)
 	    .on("mouseover", mouseOver(0.2))
 	    .on("mouseout", mouseOut);
+
+	var circles = node.append("circle")
+		.attr("r", function(d) { return d.circle.r; })
+		.attr("cluster", function(d) { return d.circle.cluster; })
+		.attr("fill", function(d) { return d.circle.fill; })
+
+
+	var labels = node.append("text")
+		.text(function(d) { return d.node.__data__.id; })
+		.style('fill', function(d) { return d.label.style.fill; })
+		.style('stroke', function(d) {return d.label.style.stroke; })
+		.attr('x', function(d) {return d.label.x; })
+		.attr('y', function(d) {return d.label.y; });
+
+
+	console.log(circles);
+
+	function mousedowned(d){
+		if (shiftKey) {
+			d3.select(this).classed("selected", d.selected = !d.selected);
+			d3.event.stopImmediatePropagation();
+		} else if (!d.selected) {
+			node.classed("selected", function(p) { return p.selected = d === p;});
+		}	
+	}
+
+	// build a dictionary of nodes that are linked
+    var linkedByIndex = {};
+    links.forEach(function(d) {
+        linkedByIndex[d.__data__.source.id + "," + d.__data__.target.id] = 1;
+    });
+
+    // check the dictionary to see if nodes are linked
+    function isConnected(a, b) {
+        return linkedByIndex[a.node.__data__.id + "," + b.node.__data__.id] || linkedByIndex[b.node.__data__.id + "," + a.node.__data__.id] || a.node.__data__.id == b.node.__data__.id;
+    }
+
+    // fade nodes on hover
+    function mouseOver(opacity) {
+        return function(d) {
+            // check all other nodes to see if they're connected
+            // to this one. if so, keep the opacity at 1, otherwise
+            // fade
+            node.style("stroke-opacity", function(o) {
+                thisOpacity = isConnected(d, o) ? 1 : opacity;
+                return thisOpacity;
+            });
+            node.style("fill-opacity", function(o) {
+                thisOpacity = isConnected(d, o) ? 1 : opacity;
+                return thisOpacity;
+            });
+            // also style link accordingly
+            link.style("stroke-opacity", function(o) {
+                return o.__data__.source.id === d.node.__data__.id || o.__data__.target.id === d.node.__data__.id ? 1 : opacity;
+            });
+            link.style("stroke", function(o){
+                return o.__data__.source.id === d.node.__data__.id || o.__data__.target.id === d.node.__data__.id ? o.__data__.source.colour : "#ddd";
+            });
+        };
+    }
+
+    function mouseOut() {
+        node.style("stroke-opacity", 1);
+        node.style("fill-opacity", 1);
+        link.style("stroke-opacity", 1);
+        link.style("stroke", "#ddd");
+    }
 }
