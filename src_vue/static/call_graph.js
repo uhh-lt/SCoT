5b085c3b9347ce68dd827427ed8175480393c80a
Inga Kempfert <5kempfer@informatik.uni-hamburg.de>
2019-12-18 09:53:01 +0100

    delete call_graph.js - never used

--- src_vue/static/call_graph.js
+++
@@ -1,6 +0,0 @@
-// NOT NEEDED OR USED CAN BE DELETED
-
-function call_graph() {
-	var url = '/sense_graph' + '/' + encodeURIComponent(target_word) + '/' + start_year + '/' + end_year + '/' + direct_neighbours + '/' + density + '/' + mode
-	render_graph(url);
-}
